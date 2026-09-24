6d99f1c92911973e902819a1d9713913031f5448
Mcalles13 <oscarmcalles@gmail.com>
2026-09-24 15:49:01 +0200

    Change image extensions from .jpg to .jpeg
    Updated image file extensions for products.

--- productos.js
+++ productos.js
@@ -2,14 +2,14 @@ const productos = [
     {
        nombre: "Kung Fu Panda PS3",
         precio: "10,00 €",
-        imagen: "ps3-kung fu panda.jpg",
+        imagen: "ps3-kung fu panda.jpeg",
         enlace: "https://es.wallapop.com/item/kung-fu-panda-el-videojuego-ps3-1287429309",
         categoria: "ps3"
     },
     {
         nombre: "Virtua Tennis 4",
         precio: "10,00€",
-        imagen: "ps3-virtuatennis4.jpg",
+        imagen: "ps3-virtuatennis4.jpeg",
         enlace: "https://es.wallapop.com/item/juego-ps3-virtual-tennis-4-1287424554",
         categoria: "ps3"
     },
